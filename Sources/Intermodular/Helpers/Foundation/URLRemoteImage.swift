4a878bb6cb62a8a5527a6f56b0f77beb589980f5
Vatsal Manot <vatsal.manot@yahoo.com>
2020-03-06 04:49:08 +0530

    Update package

--- Sources/Intermodular/Helpers/Foundation/URLRemoteImage.swift
+++ Sources/Intermodular/Helpers/Foundation/URLRemoteImage.swift
@@ -6,52 +6,67 @@ import Merge
 import Foundation
 import SwiftUIX
 
-public struct URLRemoteImage: ViewModifier {
-    let url: URL
-    let cacheInUserDefaults: Bool
+public struct URLRemoteImage<Placeholder: View>: View {
+    let placeholder: Placeholder
+    let url: URL?
+    let urlSession: URLSession = .images
     
-    @State var image: Image?
+    @State private var image: Image?
     
-    var userDefaultsKey: String {
-        "URLImageLoader.\(self.url.absoluteString)"
-    }
-    
-    public init(url: URL, cacheInUserDefaults: Bool) {
+    public init(url: URL?, @ViewBuilder placeholder: () -> Placeholder) {
         self.url = url
-        self.cacheInUserDefaults = cacheInUserDefaults
+        self.placeholder = placeholder()
     }
     
-    public func body(content: Content) -> some View {
+    private func dataPublisher() -> AnyPublisher<Data?, Never> {
+        if let url = url {
+            return urlSession
+                .dataTaskPublisher(for: url)
+                .map { $0.data }
+                .replaceError(with: nil)
+                .receive(on: DispatchQueue.main)
+                .eraseToAnyPublisher()
+        } else {
+            return Just(nil)
+                .eraseToAnyPublisher()
+        }
+    }
+    
+    public var body: some View {
         image.ifSome { image in
             image.resizable()
         }.else {
-            content.onReceive(
-                URLSession.shared
-                    .dataTaskPublisher(for: url)
-                    .receiveOnMainQueue()
-                    .toResultPublisher()
-            ) { result in
-                if case let .success(value) = result {
-                    self.image = Image(data: value.data)
-                    
-                    UserDefaults.standard.set(value.data, forKey: self.userDefaultsKey)
-                }
-            }.onAppear {
-                if let data = UserDefaults.standard.data(forKey: self.userDefaultsKey) {
-                    self.image = Image(data: data)
-                }
+            placeholder
+                .onReceive(dataPublisher()) { output in
+                    self.image = output.flatMap(Image.init(data:))
             }
         }
     }
 }
 
-// MARK: - Helpers -
+private extension URLSession {
+    static let images: URLSession = {
+        let cachePath = try? FileManager.default.url(
+            for: .cachesDirectory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: true
+        ).absoluteString
+        
+        let cache = URLCache(memoryCapacity: 1_024 * 1_024 * 8, diskCapacity: 1_024 * 1_024 * 64, diskPath: cachePath)
+        let configuration = URLSessionConfiguration.default
+        
+        configuration.urlCache = cache
+        configuration.requestCachePolicy = .useProtocolCachePolicy
+        
+        return URLSession(configuration: configuration)
+    }()
+}
 
 extension View {
-    public func remoteImage(
-        from url: URL,
-        cacheInUserDefaults: Bool = true
-    ) -> some View {
-        modifier(URLRemoteImage(url: url, cacheInUserDefaults: cacheInUserDefaults))
+    public func remoteImage(from url: URL?) -> some View {
+        URLRemoteImage(url: url) {
+            self
+        }
     }
 }
