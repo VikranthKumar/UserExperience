c2257413a3681b2912b49e8625a430566f8930b2
Vatsal Manot <vatsal.manot@yahoo.com>
2020-03-05 18:34:12 +0530

    Update package

--- Sources/Intermodular/Helpers/Foundation/URLRemoteImage.swift
+++ Sources/Intermodular/Helpers/Foundation/URLRemoteImage.swift
@@ -7,15 +7,40 @@ import Foundation
 import SwiftUIX
 
 public struct URLRemoteImage: ViewModifier {
-    @ObservedObject var loader: URLImageLoader
+    let url: URL
+    let cacheInUserDefaults: Bool
     
-    public init(loader: URLImageLoader) {
-        self.loader = loader
+    @State var image: Image?
+    
+    var userDefaultsKey: String {
+        "URLImageLoader.\(self.url.absoluteString)"
+    }
+    
+    public init(url: URL, cacheInUserDefaults: Bool) {
+        self.url = url
+        self.cacheInUserDefaults = cacheInUserDefaults
     }
     
     public func body(content: Content) -> some View {
-        ZStack {
-            loader.image.map({ $0.resizable() }) ?? content
+        image.ifSome { image in
+            image.resizable()
+        }.else {
+            content.onReceive(
+                URLSession.shared
+                    .dataTaskPublisher(for: url)
+                    .receiveOnMainQueue()
+                    .toResultPublisher()
+            ) { result in
+                if case let .success(value) = result {
+                    self.image = Image(data: value.data)
+                    
+                    UserDefaults.standard.set(value.data, forKey: self.userDefaultsKey)
+                }
+            }.onAppear {
+                if let data = UserDefaults.standard.data(forKey: self.userDefaultsKey) {
+                    self.image = Image(data: data)
+                }
+            }
         }
     }
 }
@@ -27,11 +52,6 @@ extension View {
         from url: URL,
         cacheInUserDefaults: Bool = true
     ) -> some View {
-        modifier(URLRemoteImage(
-            loader: .init(
-                url: url,
-                cacheInUserDefaults: cacheInUserDefaults
-            )
-        ))
+        modifier(URLRemoteImage(url: url, cacheInUserDefaults: cacheInUserDefaults))
     }
 }
